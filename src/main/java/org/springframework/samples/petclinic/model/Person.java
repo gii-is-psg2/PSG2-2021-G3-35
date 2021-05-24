ded2f871214d78415e4b3001a892efd1126d8550
djd16 <diego_16_rey@hotmail.com>
2021-05-24 20:29:17 +0200

    Task #95-I-964 finished
    Corrección en clase persona para que no haya números en los nombres

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 /**
  * Simple JavaBean domain object representing an person.
@@ -29,10 +30,12 @@ public class Person extends BaseEntity {
 
 	@Column(name = "first_name")
 	@NotEmpty
+	@Pattern(regexp = "[^0-9]*", message = "Introduce un nombre correcto / Introduce a First Name correct")
 	protected String firstName;
 
 	@Column(name = "last_name")
 	@NotEmpty
+	@Pattern(regexp = "[^0-9]*", message = "Introduce un Apellido correcto / Introduce a Second Name correct")
 	protected String lastName;
 
 	public String getFirstName() {
